6a28971ea8932dfe55c5cb3a88592dff41bd76a4
luca de carne <luca.decarne@edu.unito.it>
2021-07-05 09:31:51 +0200

    Fixing an error when validating the authorization header

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,13 +4,14 @@ const ApiError = require('../utils/ApiError');
 
 const verifyAccessToken = async (req, next) => {
   const checkTokenInfo = 'oauth2/v1/tokeninfo';
+  if (!req.header('Authorization')) return next(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
   const token = req.header('Authorization').split(' ')[1];
   const reqURL = new URL(`https://www.googleapis.com/${checkTokenInfo}`);
   reqURL.searchParams.append('access_token', token);
   const response = await fetch(reqURL.toString());
   const serverRes = await response.json();
-  if (serverRes.error) next(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
-  else next();
+  if (serverRes.error) return next(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+  return next();
 };
 
 const auth = () => async (req, res, next) => {
